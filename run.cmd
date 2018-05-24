1a07859f1a0fd4e83d6907806f360049e168bb9c
רועי <benmen1@gmail.com>
2018-05-24 17:47:27 +0300

    remove -noexit
    remove the -noexit parametr from the powershell excution command in the bat file.

--- run.cmd
+++ run.cmd
@@ -1,6 +1,6 @@
 @echo off & setlocal
 set @batchPath=%~dp0
 set @filePath="%@batchPath%sendmail.ps1"
-echo %@filePath%
-powershell.exe -noexit -file %@filePath% -ToEmail roy@roi-holdings.com -Subject This is test mail from Priority
-pause
+set @Subject="This is test mail from Priority"
+REM if you want to see the code add -noexit before the -file
+START "" powershell.exe  -file %@filePath% -ToEmail roy@roi-holdings.com  -Subject %@Subject%
